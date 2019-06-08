30eadb355a5481d40b9d3cbad56327507eb5deb0
Cristian <3py94.cg@gmail.com>
2019-06-08 02:09:25 -0500

    adding access type view

--- src/components/AccessType/index.jsx
+++ src/components/AccessType/index.jsx
@@ -3,61 +3,118 @@ import { makeStyles } from "@material-ui/core/styles";
 import {
   Card,
   CardContent,
-  Button,
+  Fab,
   Typography,
-  CardActions
+  Grid,
+  Box,
+  Divider
 } from "@material-ui/core";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   card: {
-    minWidth: 275
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)"
+    minWidth: 420,
+    maxWidth: "min-content",
+    borderRadius: 12,
+    padding: theme.spacing(3, 2),
+    boxShadow: "5px 5px 8px 3px rgba(0,0,0,0.1)",
+    border: "none"
   },
   title: {
-    fontSize: 14
+    fontSize: 16,
+    fontFamily: "Roboto Mono, monospace",
+    textAlign: "right"
   },
-  pos: {
-    marginBottom: 12
+  imgContainer: {
+    width: 150
+  },
+  img: {
+    width: "100%"
+  },
+  actions: {
+    padding: theme.spacing(5, 0),
+    justifyContent: "center"
+  },
+  btn: {
+    width: "70%",
+    margin: theme.spacing(1, 0),
+    background: "none",
+    color: "#707070",
+    border: "1px solid #707070",
+    boxShadow: "none",
+    "&:hover": {
+      color: "white",
+      border: "1px solid #707070",
+      background: "#707070",
+      
+    }
   }
-});
+}));
 
 export default function AccessType() {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
-
   return (
     <Card className={classes.card}>
       <CardContent>
-        <Typography
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
         >
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be
-          {bull}
-          nev
-          {bull}o{bull}
-          lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+          <Box className={classes.imgContainer}>
+            <img
+              className={classes.img}
+              alt="complex"
+              src={process.env.PUBLIC_URL + "/images/Logo-Helper.png"}
+            />
+          </Box>
+
+          <Typography className={classes.title} color="textSecondary">
+            <strong>BIENVENIDO</strong> <br />
+            Perfil de acceso
+          </Typography>
+        </Grid>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+      <Divider variant="middle" />
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        className={classes.actions}
+      >
+        <Fab
+          className={classes.btn}
+          variant="extended"
+          color="primary"
+          aria-label="Add"
+        >
+          Administrativo
+        </Fab>
+        <Fab
+          className={classes.btn}
+          variant="extended"
+          color="primary"
+          aria-label="Add"
+        >
+          Profesor
+        </Fab>
+        <Fab
+          className={classes.btn}
+          variant="extended"
+          color="primary"
+          aria-label="Add"
+        >
+          Padre o Estudiante
+        </Fab>
+        <Fab
+          className={classes.btn}
+          variant="extended"
+          color="primary"
+          aria-label="Add"
+        >
+          Asociación
+        </Fab>
+      </Grid>
     </Card>
   );
 }
--- src/views/AccessView/index.jsx
+++ src/views/AccessView/index.jsx
@@ -1,28 +1,22 @@
 import React, { Component } from "react";
 import AccessType from "../../components/AccessType";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import { Grid } from "@material-ui/core";
 
-
-
-// const classes = useStyles();
-export default class index extends Component {
- 
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-      useStyles: makeStyles({
-        cstContainer: {
-          height: "100vh"
-        }
-      })
-    }
+const styles = {
+  cstContainer: {
+    height: "100vh",
+    background: "rgba(52,147,230,1);" /* fallback for old browsers */,
+    background:
+       `url('${process.env.PUBLIC_URL}/images/bg-pattern.png'), -moz-linear-gradient(-45deg, rgba(52,147,230,1) 0%, rgba(97,129,219,1) 28%, rgba(113,123,215,1) 38%, rgba(196,91,194,1) 89%);`,
+    background:
+    `url('${process.env.PUBLIC_URL}/images/bg-pattern.png'), -webkit-linear-gradient(-45deg, rgba(52,147,230,1) 0%, rgba(97,129,219,1) 28%, rgba(113,123,215,1) 38%, rgba(196,91,194,1) 89%)`,
+    backgroundSize: "cover"
   }
-  
-
+};
+class index extends Component {
   render() {
-    const classes = this.state.useStyles()
+    const { classes } = this.props;
     return (
       <Grid
         className={classes.cstContainer}
@@ -36,3 +30,4 @@ export default class index extends Component {
     );
   }
 }
+export default withStyles(styles)(index);
